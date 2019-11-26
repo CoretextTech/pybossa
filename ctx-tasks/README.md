a319f86200af9241680866f38198277521bcb593
Vladimir Zorin <csum@mail.ru>
2019-11-26 16:31:12 +0400

    Pybossa tasks package added.

---
+++ ctx-tasks/README.md
@@ -0,0 +1,11 @@
+# Pybossa tasks
+
+NodeJS v10+ required
+
+<code>npm install</code> first
+
+<code>npm test</code> to run all tests and validations
+
+## Input / Output JSON schemas
+
+<code>npm run test_schema</code> to validate all test input and output JSON data in <i>/tasks</i> folder
---
+++ ctx-tasks/test-schema.js
@@ -0,0 +1,23 @@
+const Ajv = require('ajv');
+const fs = require('fs');
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+const tasks = [
+  'binary_classification',
+  'category_classification',
+]
+
+tasks.forEach(t => {
+  const inputSchema = JSON.parse(fs.readFileSync(`./tasks/${t}/input.schema.json`));
+  const outputSchema = JSON.parse(fs.readFileSync(`./tasks/${t}/output.schema.json`));
+  const testInput = JSON.parse(fs.readFileSync(`./tasks/${t}/test/input.json`));
+  const testOutput = JSON.parse(fs.readFileSync(`./tasks/${t}/test/output.json`));
+
+  const validInput = ajv.validate(inputSchema, testInput)
+  !validInput && console.log(t, 'invalid input:', ajv.errorsText())
+
+  const validOutput = ajv.validate(outputSchema, testOutput)
+  !validOutput && console.log(t, 'invalid output:', ajv.errorsText())
+
+  validInput && validOutput && console.log('Test data is valid in', t, 'task', '\r\n');
+})
